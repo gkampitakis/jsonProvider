e99d764a738f47e40e043cd2c92a89df6d6eab68
George Kampitakis <gkabitakis@gmail.com>
2019-11-22 20:16:39 +0000

    Add missing script for Heroku deployment

--- src/config/express.ts
+++ src/config/express.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import errorHandler from 'errorhandler';
 import methodOverride from 'method-override';
 import cors from 'cors';
 import { config } from './environment';
@@ -8,14 +7,11 @@ import { config } from './environment';
 export function setupExpress(app: express.Application) {
 
   const env: string = config.env;
+  console.log(env);
+
 
   app.use(cors());
   app.use(bodyParser.urlencoded({ extended: false }));
   app.use(bodyParser.json());
   app.use(methodOverride());
-
-  if (env === 'development') {
-    app.use(errorHandler()); // Error handler - has to be last
-  }
-
 }
