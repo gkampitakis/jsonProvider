2c589ef8e4d9b2debe6b602d754c0121e48010b4
George Kampitakis <gkabitakis@gmail.com>
2020-01-18 21:22:38 +0000

    Token service tests

--- src/api/auth/token/token.service.test.ts
+++ src/api/auth/token/token.service.test.ts
@@ -1,8 +1,109 @@
-describe('Testing test', () => {
-  //TODO: implement tests
-  it('test 1', () => {
+import mongoose from 'mongoose';
+import { Token } from "./token.model";
+import { TokenService } from "./token.service";
 
-    expect(true).toBe(true);
+let connection,
+  token: string;
+
+const tokenService = new TokenService(),
+  user_Id = '5e190be690b8fb10d070dffe';
+
+describe('Token Create', () => {
+
+  beforeAll(async () => {
+
+    connection = await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      useCreateIndex: true
+    });
+
+  });
+
+  afterAll(async () => {
+
+    await connection.close();
+
+  });
+
+  afterEach(async () => {
+
+    await Token.deleteMany({});
+
+  });
+
+  it("should create a token containing the provided userId", async () => {
+
+    const { token, userId } = await tokenService.create(user_Id, 'authorization');
+
+    expect(token).not.toBe(undefined);
+    expect(user_Id).toEqual(userId);
+
+  });
+
+});
+
+describe('Remove Token', () => {
+
+  beforeEach(async () => {
+
+    await tokenService.create(user_Id, 'authorization');
+
+  });
+
+  it("should remove token", async () => {
+
+    await tokenService.remove(user_Id);
+
+    try {
+
+      await tokenService.retrieve(user_Id);
+
+    } catch (error) {
+
+      expect(error.message).toBe('Token not found');
+
+    }
+
+  });
+
+});
+
+
+describe('Retrieve Token', () => {
+
+  beforeEach(async () => {
+
+    await tokenService.create(user_Id, 'authorization');
+
+  });
+
+  it("should remove token", async () => {
+
+
+    const token = await tokenService.retrieve(user_Id);
+
+    expect(token).not.toBe(undefined);
+
+  });
+
+});
+
+describe('Retrieve Verification Token', () => {
+
+  beforeEach(async () => {
+
+    const result = await tokenService.create(user_Id, 'verification');
+    token = result.token;
+
+  });
+
+  it("should retrieve token", async () => {
+
+    const result = await tokenService.retrieveVerificationToken(token);
+
+    expect(result).not.toBe(undefined);
 
   });
 
